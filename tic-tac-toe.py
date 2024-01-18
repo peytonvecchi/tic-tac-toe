603c8d45f7f583ac0131663cea7d40d16563bc67
Peyton <74024201+veechi@users.noreply.github.com>
2024-01-18 16:29:51 -0500

    Upload to GitHub

---
+++ tic-tac-toe.py
@@ -0,0 +1,25 @@
+# Need two players
+
+class Game:
+    def __init__(self):
+        pass
+
+
+'''
+Flow
+
+1. ask user if they would like to play, let them know they need
+another person
+
+2. prompt player1 to enter name and if they want X or O
+
+3. prompt player2 to enter their name and let them know which symbol they are
+
+4. prompt user if they would like instructions on how to play, then print instructions
+
+5. wait for user to press any key to continue
+
+6. flip a coin to see who goes first
+
+'''
+
